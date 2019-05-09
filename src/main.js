ad31133afcb916641db3b38cba5c2ccf1ba8bc23
zuriros <hanami33rosas@gmail.com>
2019-05-09 00:07:41 -0500

    pequeños cambios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {routerVN} from '../src/router.js';
 
 const init = () => {
-    routerVN ();
-}
-window.addEventListener('load',init)
+  routerVN();
+};
+window.addEventListener('load', init);
